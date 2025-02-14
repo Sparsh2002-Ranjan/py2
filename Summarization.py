4a0ae704c4cf5cfe81f9126a98207a235d48afcf
Sparsh2002-Ranjan <ranjansparsh872@gmail.com>
2025-02-14 23:11:04 +0530

    Update Summarization.py

--- Summarization.py
+++ Summarization.py
@@ -52,5 +52,5 @@ def summarize_text():
 app.register_blueprint(summarization_bp)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
